c51b8076b6a7c6c9fb4cc427181c6d3da93e2b91
AlexLaur <laurette.alexandre@gmail.com>
2021-03-20 19:03:26 +0100

    Convert ZigZag indicator to customisable

--- app/add_ons/indicators/zig_zag.py
+++ app/add_ons/indicators/zig_zag.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from scipy import signal
 
-from libs.indicators_widget import Indicator
+from libs.indicators_widget import Indicator, InputField
 
 
 class ZigZag(Indicator):
@@ -13,18 +13,30 @@ class ZigZag(Indicator):
         self.name = "ZigZag"
         self.description = ""
 
+        # Define and register all customisable settings
+        field_zigzag = InputField("ZigZag", color=(33, 150, 243), width=2.5)
+        self.register_field(field_zigzag)
+
     def create_indicator(self, graph_view, *args, **kwargs):
+        super(ZigZag, self).create_indicator(graph_view)
+
         # Get values
         values = graph_view.values
         quotation_plot = graph_view.g_quotation
 
+        # Retrive settings
+        field_zigzag = self.get_field("ZigZag")
+
+        # Calculation
         zigzag = zig_zag(values=values["Close"].values)
+
+        # Draw plot
         plot = quotation_plot.plot(
             x=[x.timestamp() for x in values.index[zigzag]],
             y=values["Close"].values[zigzag],
-            pen=pg.mkPen((33, 150, 243), width=2.5),
+            pen=pg.mkPen(field_zigzag.color, width=field_zigzag.width),
         )
-        self._plots.append(plot)
+        self.register_plot(plot)
 
 
 def zig_zag(values, distance=2.1):
