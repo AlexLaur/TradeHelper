5644991e0f27cdd2a5a249a2526a0988a4b07806
AlexLaur <laurette.alexandre@gmail.com>
2021-03-20 17:37:17 +0100

    Convert bollingers band to customisable

--- app/add_ons/indicators/bollinger_bands.py
+++ app/add_ons/indicators/bollinger_bands.py
@@ -1,6 +1,6 @@
 import pyqtgraph as pg
 
-from libs.indicators_widget import Indicator
+from libs.indicators_widget import Indicator, InputField
 
 
 class BollingerBands(Indicator):
@@ -11,37 +11,54 @@ class BollingerBands(Indicator):
         self.description = ""
 
         self.g_filler = None
-        self.color = (0, 140, 170, 50)
+
+        # Define and register all customisable settings
+        field_middle = InputField("Middle", color=(0, 140, 170), width=1.2)
+        field_upper = InputField("Upper", color=(0, 140, 170), width=1.2)
+        field_lower = InputField("Lower", color=(0, 140, 170), width=1.2)
+        field_filler = InputField("Fill Between", color=(0, 140, 170, 50))
+        self.register_fields(field_middle, field_upper, field_lower, field_filler)
 
     def create_indicator(self, graph_view, *args, **kwargs):
+        super(BollingerBands, self).create_indicator(graph_view)
+
         # Get values
         values = graph_view.values
         quotation_plot = graph_view.g_quotation
 
+        # Retrive settings
+        field_middle = self.get_field("Middle")
+        field_upper = self.get_field("Middle")
+        field_lower = self.get_field("Lower")
+        field_filler = self.get_field("Fill Between")
+
+        # Calculate
         middler, upper, lower = bollinger_bands(values)
 
+        # Create plots
         middler_plot = quotation_plot.plot(
             x=[x.timestamp() for x in values.index],
             y=middler,
-            pen=pg.mkPen(color=self.color[0:3], width=1.2),
+            pen=pg.mkPen(color=field_middle.color, width=field_middle.width),
         )
+
         upper_plot = quotation_plot.plot(
             x=[x.timestamp() for x in values.index],
             y=upper,
-            pen=pg.mkPen(color=self.color[0:3], width=1.2),
+            pen=pg.mkPen(color=field_upper.color, width=field_upper.width),
         )
+
         lower_plot = quotation_plot.plot(
             x=[x.timestamp() for x in values.index],
             y=lower,
-            pen=pg.mkPen(color=self.color[0:3], width=1.2),
+            pen=pg.mkPen(color=field_lower.color, width=field_lower.width),
         )
 
         self.g_filler = pg.FillBetweenItem(
-            curve1=upper_plot, curve2=lower_plot, brush=pg.mkBrush(self.color)
+            curve1=upper_plot, curve2=lower_plot, brush=pg.mkBrush(field_filler.color)
         )
 
         quotation_plot.addItem(self.g_filler)
-
         self._plots.extend([lower_plot, middler_plot, upper_plot])
 
     def remove_indicator(self, graph_view, *args, **kwargs):
@@ -52,9 +69,9 @@ class BollingerBands(Indicator):
         self.g_filler = None
 
 
-def bollinger_bands(values):
-    middle_band = values["Close"].rolling(window=20).mean()
-    standard_deviation = values["Close"].rolling(window=20).std()
+def bollinger_bands(values, window=20):
+    middle_band = values["Close"].rolling(window=window).mean()
+    standard_deviation = values["Close"].rolling(window=window).std()
 
     upper_band = middle_band + standard_deviation * 2
     lower_band = middle_band - standard_deviation * 2
